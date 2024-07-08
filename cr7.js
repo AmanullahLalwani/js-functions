b83eb84a4d08aec8053660afc1ade551de41250d
AmanullahLalwani <amanullahamjad342@gmail.com>
2024-07-08 13:01:15 +0500

    function,arowfunction

---
+++ cr7.js
@@ -0,0 +1,22 @@
+
+
+function nice(name) {
+    console.log("hey " + name + " your good player")
+    console.log("hey " + name + " your bollon dor champ")
+    console.log("hey " + name + " your number 7")
+    console.log("hey " + name + " your king of football")
+}
+
+nice("aman")
+nice("messi")
+nice("cristiano")
+
+// arrow function
+
+ func1 =(x)=>{
+    console.log("i am a number" , x)
+ }
+
+ func1(55)
+ func1(88)
+ func1(30)
